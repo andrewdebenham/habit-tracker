d0f7e4274f9d182c7f66be72d24736285df9899c
andrewdebenham <andrew.debenham94@gmail.com>
2024-12-03 18:25:11 +0000

    created calendar component

--- habit-tracker-frontend/screens/HomeScreen.js
+++ habit-tracker-frontend/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { useAuthedUser } from '../contexts/AuthedUserProvider';
+import Calendar from '../components/Calendar';
 import { logout } from '../services/authService';
 
 function HomeScreen() {
@@ -14,6 +15,7 @@ function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Welcome to the Home Screen!</Text>
+      <Calendar />
       <Button title="Logout" onPress={handleLogout} />
     </View>
   );
@@ -22,7 +24,7 @@ function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    paddingTop: 10,
     alignItems: 'center',
     backgroundColor: '#fff',
   },
